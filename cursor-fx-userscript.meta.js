ed9355d932338a6b9803aba13736a81bbc6ba46d
高新炀 <gao_xinyang@foxmail.com>
2026-07-26 22:04:50 +0800

    Create cursor-fx-userscript.meta.js

---
+++ cursor-fx-userscript.meta.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name         Cursor FX
+// @namespace    https://github.com/Xinyang-Gao/cursor-fx-userscript
+// @version      2.0.0
+// @description  Smooth custom cursor userscript.
+// @author       Xinyang-Gao
+// @downloadURL  https://raw.githubusercontent.com/Xinyang-Gao/cursor-fx-userscript/main/cursor-fx-userscript.user.js
+// @updateURL    https://raw.githubusercontent.com/Xinyang-Gao/cursor-fx-userscript/main/cursor-fx-userscript.meta.js
+// ==/UserScript==
